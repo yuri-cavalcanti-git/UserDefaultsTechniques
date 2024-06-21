365d6f945500c32cbde95ec6299261f2ff8fb3d0
Yuri Cavalcanti <yuri.cavalcanti2013@gmail.com>
2024-06-21 11:23:30 -0300

    add project

---
+++ UserDefaultsTechniques/UserDefaultsTechniques/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDefaultsTechniques
+//
+//  Created by Yuri Cavalcanti on 21/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
